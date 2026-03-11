66173cd833b316b81761c68cf6c5440f8f752731
Maanee-dev <husn.maanee@gmail.com>
2026-03-11 13:14:48 +0500

    feat: Update villa image URLs and add new gallery images
    Replaces outdated image URLs for Garden Villa and Beach Villa with more relevant ones. Additionally, adds a new gallery image to the Garden Villa to enhance its visual representation.

--- src/data.ts
+++ src/data.ts
@@ -28,7 +28,8 @@ export const ayadaData: ResortData = {
       image: 'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1680344545031-LG6OKGX6EQNO33T96S7V/Garden+Villa+1.jpg?format=2500w',
       images: [
         'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1680344545031-LG6OKGX6EQNO33T96S7V/Garden+Villa+1.jpg?format=2500w',
-        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533283180231-1100ER779RL5L3HOOB3L/garden_villa2.jpg?format=2500w'
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533283180231-1100ER779RL5L3HOOB3L/garden_villa2.jpg?format=2500w',
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1680344545031-LG6OKGX6EQNO33T96S7V/Garden+Villa+1.jpg?format=2500w'
       ],
       capacity: '2 Adults + 1 Child',
       description: 'Nestled in the lush tropical gardens, these villas offer a private sanctuary with a spacious veranda and a private pool.',
@@ -38,18 +39,124 @@ export const ayadaData: ResortData = {
       id: 'beach-villa',
       name: 'Beach Villa with Pool',
       size: '103 sqm',
-      image: 'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533284612030-82FMW3ZFNZ6TYYNDU7QN/bv2.jpg?format=2500w',
+      image: 'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1532678056046-2I393HL258IGMVG5LB7Z/351bbf9b32ea226d4294d111dad38ed0.jpg?format=2500w',
+       images: [
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1532678062056-MBU0DEZ6A5O7SJB6DTSC/beach-villas-img4.jpg?format=2500w',
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1532678097920-UBU3UWXGCMG3SVO79R7D/Ayada-Maldives-villas-BEACH-VILLA-1.jpg?format=2500w'
+      ],
       capacity: '2 Adults + 1 Child',
-      description: 'Located directly on the beach, these villas feature a private pool and a large outdoor bathroom with a bathtub and rain shower.'
+      description: 'Located directly on the beach, these villas feature a private pool and a large outdoor bathroom with a bathtub and rain shower.',
+      map_url: 'https://www.google.com/maps/embed?pb=!4v1533289383476!6m8!1m7!1sCAoSLEFGMVFpcFBWRDQ0SWwzTEtBTENwQVh3M3F2bXZNNmJjQ0YzSUE2UGxGR2d2!2m2!1d0.2776306007460441!2d73.35778663788881!3f265.05!4f-14.480000000000004!5f0.5970117501821992'
     },
-    {
-      id: 'ocean-villa',
+   {
+      id: 'beach-suite',
+      name: 'Beach suite with pool',
+      size: '115 sqm',
+      image: 'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1534935258602-B5QKG7F1TYECM2I6I6KD/Ayada+Maldives+Villas+BEACH+SUITE+%282%29.jpg?format=2500w',
+       images: [
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1534935258020-KA8SFV40EG1JHPQBR4G0/Ayada+Maldives+Villas+BEACH+SUITE+%283%29.jpg?format=2500w',
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533282713836-BQ7JU7T2AT32W85JFOZQ/sbs1.jpg?format=2500w'
+      ],
+      capacity: '3 adults or 2 adults + 2 children',
+      description: 'Located on beautiful white sand beaches. Large bedroom, spacious veranda with sunbeds, plunge pool in private garden. Ideal for families or couples preferring larger living space. Bathroom with bathtub and large back courtyard with outdoor shower.',
+      map_url: 'https://www.google.com/maps/embed?pb=!4v1533289383476!6m8!1m7!1sCAoSLEFGMVFpcFBWRDQ0SWwzTEtBTENwQVh3M3F2bXZNNmJjQ0YzSUE2UGxGR2d2!2m2!1d0.2776306007460441!2d73.35778663788881!3f265.05!4f-14.480000000000004!5f0.5970117501821992'
+    },
+      {
+      id: 'sunset-beach-suite',
+      name: 'Sunset Beach Suite with Pool',
+      size: '137 sqm',
+      image: 'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533282363375-VBLFVYO1PKXVFL7OICJU/Ayada+Maldives+villas+SUNSET+BEACH+SUITE+%282%29.jpg?format=2500w',
+       images: [
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533282498207-BX02S40S9F76XNHUWYZJ/sbs2.jpg?format=2500w',
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533282548114-JZ82R9XKLZF9REYWO5NB/sbs5.jpg?format=2500w'
+      ],
+      capacity: '3 adults or 2 adults + 2 children',
+      description: 'Spacious beachfront suites with large living space, indoor and outdoor shower, spacious outdoor terrace, secluded garden with private pool and direct beach access. Located where sunsets are most beautiful.',
+      map_url: 'https://www.google.com/maps/embed?pb=!4v1533289588819!6m8!1m7!1sCAoSLEFGMVFpcFBob0o0MmFqaWNrZEtYQlY5OTBPYkFnM200b2lwLWlVMVNqU3VS!2m2!1d0.2774525621663824!2d73.35787217292955!3f269.77!4f-5.069999999999993!5f0.5970117501821992'
+    },
+        {
+      id: 'sunset-lagoon-suite',
+      name: 'Sunset Lagoon Suite with Pool',
+      size: '156 sqm',
+      image: 'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533275941818-K7CI7DFL3EAP99UKCVRJ/Ayada+Maldives+villas+SUNSET+LAGOON+SUITE+%281%29.jpg?format=2500w',
+       images: [
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533276224141-XNPEP2EQJH6H4TJSJWTJ/Ayada+Maldives+Sunset+Lagoon+Suite+Pool.jpg?format=2500w',
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533275995361-SCI91SCA3OE8YY2SNWU1/Ayada+Maldives+villas+SUNSET+LAGOON+SUITE+%282%29.jpg?format=2500w'
+      ],
+      capacity: '3 adults or 2 adults + 2 children',
+      description: 'Resting on land with outdoor terrace over ocean, combining tropical garden with magical lagoon view. Large area encompassing living and bedroom area with bathroom opening into private garden. Outdoor terraces with plush swing sofas, plunge pool, direct lagoon access.',
+      map_url: 'src="https://www.google.com/maps/embed?pb=!4v1533289849094!6m8!1m7!1sCAoSLEFGMVFpcFAtSkpQMDhUZTFoSGZaWE83QzFaU2pPLXdSUFU0aFZNTlhRMWI4!2m2!1d0.2825091893138589!2d73.35602034072963!3f66.10501662467122!4f-7.08734222006801!5f0.7820865974627469"'
+    },
+           {
+      id: 'ocean-villa-with-pool',
       name: 'Ocean Villa with Pool',
       size: '108 sqm',
-      image: 'https://picsum.photos/seed/ocean/800/600',
-      capacity: '2 Adults',
-      description: 'Perched over the turquoise lagoon, these villas offer direct access to the ocean and a private infinity pool on the deck.'
-    }
+      image: 'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533282066069-145RDAGJ0I93OL2TLZLM/Ayada+Maldives+villas+OCEAN+VILLA+%283%29.jpg?format=2500w',
+       images: [
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1614936160673-42JMHW0UPYDLSD5BNVEY/Ayada_Maldives059.jpg?format=2500w',
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533282064585-ULINSXG87E3D7UO8JD6F/Ayada+Maldives+villas+OCEAN+VILLA+bathroom.jpg?format=2500w'
+      ],
+      capacity: '2 adults + 1 child',
+      description: 'Poised over lagoon, allowing idyllic contemplation while lounging on ocean hammocks. Spacious villa with private outdoor terrace, ocean hammocks, plunge pool, uninterrupted view of horizon and direct lagoon access.',
+      map_url: 'https://www.google.com/maps/embed?pb=!4v1533289716797!6m8!1m7!1sCAoSLEFGMVFpcFBranNDOU5GMTdLR3VVS041WTJMTUNNblFTa29Kam5ILWVWNmRO!2m2!1d0.2783142904663271!2d73.3553233862674!3f279.7660406309005!4f-5.452429267719509!5f0.7820865974627469'
+    }, 
+               {
+      id: 'sunset-ocean-suite-with-pool',
+      name: 'Sunset Ocean Suite with Pool',
+      size: '172 sqm',
+      image: 'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533275638438-UBGTNUUSBGX41U5GN9W2/Ayada+Maldives+villas+SUNSET+OCEAN+SUITE+%281%29.jpg?format=2500w',
+       images: [
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533275629506-W4UVASMFP83IH8GB85ND/Ayada+Maldives+villas+SUNSET+OCEAN+SUITE+%283%29.jpg?format=2500w',
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533275670463-UP6TMV2IBEQP2KW5MU01/Ayada+Maldives+villas+SUNSET+OCEAN+SUITE+%286%29.jpg?format=2500w'
+      ],
+      capacity: '4 adults or 2 adults + 3 children',
+      description: 'Over-water suite offering absolute luxury and serenity over Indian Ocean. Master bedroom with private bathroom, living area with glass floor panel, additional bathroom and pantry area, huge sundeck with ocean hammocks, direct lagoon access and infinite horizon views.',
+      map_url: 'https://www.google.com/maps/embed?pb=!4v1534325321699!6m8!1m7!1sCAoSLEFGMVFpcE5XQkJmcVR1cEJUMXYwdjJHU2pfZHdkbEh3TXk5c1NhRGJEWE5I!2m2!1d0.2829964714818609!2d73.35404793152657!3f24.88!4f-2.1200000000000045!5f0.5970117501821992'
+    }, 
+                   {
+      id: 'sunset-lagoon-suite-two-bedroom',
+      name: 'Sunset Lagoon Suite with Two Bedrooms',
+      size: '172 sqm',
+      image: 'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1614852992200-DJR7M9LI1CEN88WBFJVZ/Ayada_Maldives059.jpg?format=2500w',
+       images: [
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533275995361-SCI91SCA3OE8YY2SNWU1/Ayada+Maldives+villas+SUNSET+LAGOON+SUITE+%282%29.jpg?format=2500w',
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1748930065404-9DIKJJCC1YVO5PO5ZYNK/Ayada+Maldives+Sunset+Lagoon+Suite+Pool+2.jpg?format=2500w'
+      ],
+      capacity: '4 adults or 2 adults + 3 children',
+      description: 'Ideal for families with older children. Two bedrooms (one with two single beds and shower room, master with en suite full bathroom). Huge sun deck with dining table, over-water hammocks, private pool, sun loungers, daybed and steps into ocean. Floor-to-ceiling windows with direct terrace access.',
+      map_url: 'https://www.google.com/maps/embed?pb=!4v1533289849094!6m8!1m7!1sCAoSLEFGMVFpcFAtSkpQMDhUZTFoSGZaWE83QzFaU2pPLXdSUFU0aFZNTlhRMWI4!2m2!1d0.2825091893138589!2d73.35602034072963!3f66.10501662467122!4f-7.08734222006801!5f0.7820865974627469'
+    },
+
+                  {
+      id: 'family-beach-suite-with-pool',
+      name: 'Family Beach Suite with Pool',
+      size: '172 sqm',
+      image: 'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1534935258602-B5QKG7F1TYECM2I6I6KD/Ayada+Maldives+Villas+BEACH+SUITE+%282%29.jpg?format=2500w',
+       images: [
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1748939626271-MAF99WQ1X8WI4ON70O0P/Ayada+Maldives+Villas+BEACH+SUITE+%281%29.jpg?format=2500w',
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533282713836-BQ7JU7T2AT32W85JFOZQ/sbs1.jpg?format=2500w',
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1543397802038-100SRTC1OS9SPMR2445Z/Ayada_Maldives139.jpg?format=2500w'
+      ],
+      capacity: '4 adults or 2 adults + 2 children',
+      description: 'Beach Family Suites are Beach Suites interconnected with a Beach Villa, ensuring family privacy and direct access when needed. Tropical vegetation screens private pool with sun terrace, loungers, umbrella, outdoor dining table. Indoors, king-sized canopy beds, spacious bathroom with twin sinks, indoor and outdoor showers, huge terrazzo bathtub.',
+      map_url: 'https://www.google.com/maps/embed?pb=!4v1533289588819!6m8!1m7!1sCAoSLEFGMVFpcFBob0o0MmFqaWNrZEtYQlY5OTBPYkFnM200b2lwLWlVMVNqU3VS!2m2!1d0.2774525621663824!2d73.35787217292955!3f269.77!4f-5.069999999999993!5f0.5970117501821992'
+    }, 
+                  {
+      id: 'ayada-royal-ocean-suite',
+      name: 'Ayada Royal Ocean Suite',
+      size: '318 sqm',
+      image: 'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1748949151245-H9IU4A2GERX07BDBV3OT/IMG_4292.jpg?format=2500w',
+       images: [
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533274181255-U37GR1PNTQ7FSAHUDSP4/Ayada+Maldives+Villas+ROYAL+OCEAN+SUITE+%286%29.jpg?format=2500w',
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533274084262-W40W2YRI40EBQZ9UDDFO/Ayada+Maldives+Villas+ROYAL+OCEAN+SUITE+%282%29.jpg?format=2500w',
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533274244866-13HPCBJO6G2I2IVLNH3M/Ayada+Maldives+Villas+ROYAL+OCEAN+SUITE+%288%29.jpg?format=2500w',
+        'https://images.squarespace-cdn.com/content/v1/5b4f0c8d89c17294e53d4ffc/1533274238576-KUKA9607QSD0FAGHWXRI/Ayada+Maldives+Villas+ROYAL+OCEAN+SUITE+%287%29.jpg?format=2500w'
+      ],
+      capacity: '4 adults or 2 adults + 2 children',
+      description: 'Beach Family Suites are Beach Suites interconnected with a Beach Villa, ensuring family privacy and direct access when needed. Tropical vegetation screens private pool with sun terrace, loungers, umbrella, outdoor dining table. Indoors, king-sized canopy beds, spacious bathroom with twin sinks, indoor and outdoor showers, huge terrazzo bathtub.',
+      map_url: 'https://www.google.com/maps/embed?pb=!4v1564130297527!6m8!1m7!1sCAoSLEFGMVFpcE5MVmFET2FEYTA2SDlwNThGUk1JUnRmNmxkVTdsTnRSTWlsU2NK!2m2!1d0.2830082828995951!2d73.35353217706563!3f136!4f0!5f0.4125900490817119'
+    }, 
+
   ],
   dining_venues: [
     {
